531ce2d850f58025a0c251bd5589525928637ebc
Mayra Guevara <maystef1@hotmail.com>
2019-06-08 17:32:04 -0600

    creando atributos y sus respectivas propiedades para las variables primer numero y segundo numero.

--- DataBinding_IP/DataBinding_IP/Suma.cs
+++ DataBinding_IP/DataBinding_IP/Suma.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,58 @@ using System.Threading.Tasks;
 
 namespace DataBinding_IP
 {
-    class Suma
+    class Suma : INotifyPropertyChanged
     {
+        private String primerNumero;
+        private String segundoNumero;
+        private String resultado;
+     
+        private String PrimerNumero
+        {
+            get { return primerNumero; }
+            set
+            {
+                int Numero;
+                bool resultado = int.TryParse(value, out Numero);  //tryParse intenta convertirlo en entero
+                if (resultado)
+                {
+                    primerNumero = value;
+                }
+
+                OnPropertyChanged("PrimerNumero");
+                OnPropertyChanged("Reultado");
+
+            }
+        }
+
+        private String SegundoNumero
+        {
+            get { return segundoNumero; }
+            set
+            {
+                int Numero;
+                bool resultado = int.TryParse(value, out Numero);  //tryParse intenta convertirlo en entero
+                if (resultado)
+                {
+                    segundoNumero = value;
+                }
+
+                OnPropertyChanged("SegundoNumero");
+                OnPropertyChanged("Reultado");
+
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(String property)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(property));
+            }
+        }
     }
+
+    
 }
